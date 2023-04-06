fae03be990b3c4825cdac0bfbe019ca0ec10e3cc
Lebedeva Olga <128788034+OlgaLebedevaGB@users.noreply.github.com>
2023-04-06 20:08:22 +0300

    Example 002 Hello User

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,3 +1,4 @@
-﻿string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Сonsole.WriteLine(username);
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
